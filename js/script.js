c2b2795d637a03ea31e0d73f4cdd06ad3ac580a3
Bhargav Ladumor <143379865+bhargavladumor@users.noreply.github.com>
2023-10-25 21:17:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//For normal font
+function normal(){
+    document.getElementById("text").style.fontStyle = "normal";
+    document.getElementById("text").style.fontWeight = "normal";
+    document.getElementById("text").style.textAlign = "left";
+    document.getElementById("text").style.textTransform = "capitalize";
+}
+
+// For bold text
+function f1(){
+    document.getElementById("text").style.fontWeight = "bold";
+}
+
+// For italic text
+function f2(){
+    document.getElementById("text").style.fontStyle = "italic";
+}
+
+// For left align 
+function f3(){
+    document.getElementById("text").style.textAlign = "left";
+}
+
+// For right align 
+function f4(){
+    document.getElementById("text").style.textAlign = "center";
+}
+
+// For right align 
+function f5(){
+    document.getElementById("text").style.textAlign = "right";
+}
+
+// For uppercase text
+function f6(){
+    document.getElementById("text").style.textTransform = "uppercase";
+}
+
+// For uppercase text
+function f7(){
+    document.getElementById("text").style.textTransform = "lowercase";
+}
+
+// For capitalize text
+function f8(){
+    document.getElementById("text").style.textTransform = "capitalize";
+}
+
+// For clear text
+function f9(){
+    document.getElementById("text").value = "";
+}
+
+//For download .txt file
+function f10(){
+    
+}
